4bc927280e32ba86bd754b01662fa178de40e3a3
Armando <piruk7@gmail.com>
2012-06-09 16:54:27 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuaReforma::Application.config.secret_token = '1a11fe698c379dd77bc677deda5cddbacfe37a692686b8a98cb6f833e3779750bb2a629620ee2ab669067621e5cc8961715b1c8d60f98da48133e2704bc65c8e'
