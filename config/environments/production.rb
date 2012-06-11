4055e55d512fad6dec9b24f97dac4fb034b9566e
Armando <piruk7@gmail.com>
2012-06-11 04:58:35 -0300

    AP3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,9 @@ SuaReforma::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
