ca58bc704367b1c50090f9182a6277b1dd3c4a4e
Armando <piruk7@gmail.com>
2012-06-11 22:12:55 -0300

    Adicionado getEndereco()

--- app/assets/javascripts/providers.js
+++ app/assets/javascripts/providers.js
@@ -22,3 +22,39 @@ function del_task(id){
   id = '#'+id;
   $(id).remove();
 }
+
+function getEndereco() {
+  // Se o campo CEP não estiver vazio
+  if($.trim($("#campoCep").val()) != ""){
+  //document.getElementById("load").style.display = 'block';
+  /*
+   *  Para conectar no serviço e executar o json, precisamos usar a função
+   *  getScript do jQuery, o getScript e o dataType:"jsonp" conseguem fazer o cross-domain, os outros
+   *  dataTypes não possibilitam esta interação entre domínios diferentes
+   *  Estou chamando a url do serviço passando o parâmetro "formato=javascript" e o CEP digitado no formulário
+   *  http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#cep").val()
+   */
+    $.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#campoCep").val(),function(){
+      // o getScript dá um eval no script, então é só ler!
+      //Se o resultado for igual a 1
+      if(resultadoCEP["resultado"] && resultadoCEP["bairro"] != ""){
+        // troca o valor dos elementos
+        $("#campoLogradouro").val(unescape(resultadoCEP["tipo_logradouro"])+":"+unescape(resultadoCEP["logradouro"]));
+        //$("#bairro").val(unescape(resultadoCEP["bairro"]));
+        $("#campoCidade").val(unescape(resultadoCEP["cidade"]));
+        $("#campoEstado").val(unescape(resultadoCEP["uf"]));
+        //$("#enderecoCompleto").show("slow");
+        $("#campoNumero").focus();
+        //document.getElementById("load").style.display = 'none';
+        //validate()
+      }else{
+        alert("Endereço não encontrado");
+        //$("#enderecoCompleto").show("slow");
+        return false;
+      }
+    });
+  }else{
+    alert('Antes, preencha o campo CEP!')
+    //document.getElementById("load").style.display = 'none';
+  }
+}
