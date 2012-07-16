3ab0dddf74137058807ffedcac9d7d6b6986a073
Armando <piruk7@gmail.com>
2012-07-16 14:36:08 -0300

    Comentado mailer

--- app/mailers/confirmacao.rb
+++ app/mailers/confirmacao.rb
@@ -2,11 +2,11 @@ class Confirmacao < ActionMailer::Base
   default from: "contato@suareforma.com.br"
 
   def welcome(email)
-	mail(
-		:to	=>	email
-		:subject => "Confirmação Sua Reforma",
-		:template_path => 'confirmacao',
-		:template_name => 'confirma'
-	)
+	# mail(
+	# 	:to	=>	email,
+	# 	:subject => "Confirmação Sua Reforma",
+	# 	:template_path => 'confirmacao',
+	# 	:template_name => 'confirma'
+	# )
   end
 end
