4244b9dbc649e2f7e1e63a638b622970d9cdffc4
Armando <piruk7@gmail.com>
2012-07-31 00:35:29 -0300

    Adicionado comentarios

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,15 +3,21 @@ class Task < ActiveRecord::Base
   has_and_belongs_to_many :providers
   scope :pending, where(:pending => "N")
 
+  # Retornan um array de id's a partir de um array de novas tasks
   def self.create_pending_tasks(tasks)
     ids = []
+    # Caso não receba nenhuma task
     tasks ||= []
+
+    # Pra cada task do array cria uma task e armazena o id
+    # que sera retornado pelo metodo
     tasks.each do |task|
       t = Task.create! :name => task
       t.pending = "S"
       t.save
       ids << t.id
     end
+    # ids das tasks recem criadas
     ids
   end
 end
